c11ac3eba49a23930261ce0c0e1acb8f42ee4fd2
Maia Grotepass <maiatoday@gmail.com>
2021-12-02 08:16:45 +0200

    add idiomatic implementations

--- src/main/kotlin/days/Day01.kt
+++ src/main/kotlin/days/Day01.kt
@@ -3,7 +3,7 @@ object Day01 {
     fun part1(input: List<String>): Int {
         var increasedCount = 0
         var previousDepth = 0
-        input.map { it.toInt()}
+        input.map { it.toInt() }
             .forEachIndexed { index, depth ->
                 if (index > 0) {
                     if (depth > previousDepth) increasedCount++
@@ -17,12 +17,24 @@ object Day01 {
         var increasedCount = 0
         var previousDepth = 0
         input.forEachIndexed { index, _ ->
-                if (index > 0 && index<=input.size-2) {
-                    val sumDepth = input[index-1].toInt()+input[index].toInt()+input[index+1].toInt()
-                    if (sumDepth > previousDepth && index != 1) increasedCount++
-                    previousDepth = sumDepth
-                }
+            if (index > 0 && index <= input.size - 2) {
+                val sumDepth = input[index - 1].toInt() + input[index].toInt() + input[index + 1].toInt()
+                if (sumDepth > previousDepth && index != 1) increasedCount++
+                previousDepth = sumDepth
             }
+        }
         return increasedCount
     }
+
+    fun part1Idiomatic(input: List<String>, windowSize: Int = 2): Int =
+        input.map { it.toInt() }
+            .windowed(windowSize).count { it[0] < it[1] }
+
+    fun part2Idiomatic(input: List<String>, windowSize: Int = 3): Int =
+        input.map { it.toInt() }
+            .windowed(windowSize)
+            .map {it.sum()}
+            .windowed(2)
+            .count { it[0] < it[1] }
+
 }
--- src/test/kotlin/days/Day01Test.kt
+++ src/test/kotlin/days/Day01Test.kt
@@ -2,7 +2,6 @@ package days
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Ignore
 import org.junit.Test
 import readInput
 
@@ -12,10 +11,12 @@ class Day01Test {
     @Test
     fun part1() {
         assertThat(Day01.part1(testInput), `is`(7))
+        assertThat(Day01.part1Idiomatic(testInput), `is`(7))
     }
 
     @Test
     fun part2() {
         assertThat(Day01.part2(testInput), `is`(5))
+        assertThat(Day01.part2Idiomatic(testInput), `is`(5))
     }
 }
