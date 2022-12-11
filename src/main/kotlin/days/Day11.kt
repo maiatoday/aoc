32fc363dd14721fab175f019ff46d6337d6c2d45
maia grotepass <maiatoday@gmail.com>
2022-12-11 12:03:11 +0200

    BigInteger all the things 🙈

---
+++ src/main/kotlin/days/Day11.kt
@@ -0,0 +1,144 @@
+package days
+
+import java.math.BigInteger
+
+typealias MonkeySwap = Pair<Int, List<BigInteger>>
+
+object Day11 : Day<BigInteger, String> {
+    override val number: Int = 11
+    override val expectedPart1Test: BigInteger = BigInteger.valueOf(10605)
+    override val expectedPart2Test: BigInteger = BigInteger.valueOf(2713310158)
+
+    data class Monkey(
+        val items: MutableList<BigInteger>,
+        val operation: (BigInteger) -> (BigInteger),
+        val test: (BigInteger) -> (Boolean),
+        val trueMonkey: Int,
+        val falseMonkey: Int,
+        val worryDrop: (BigInteger) -> (BigInteger) = { it / BigInteger.valueOf(3) },
+        var inspectionCount: Long = 0
+    ) {
+        fun round(doWorryDrop: Boolean = false): List<MonkeySwap> {
+//            val itemValues = items.map { operation(it) }.map { if (doWorryDrop) worryDrop(it) else it }
+            val itemValues = if (doWorryDrop) items.map { operation(it) }.map { worryDrop(it)} else items.map { operation(it) }
+            inspectionCount += itemValues.size
+            val trueList = itemValues.filter { test(it) }
+            val falseList = itemValues.filter { !test(it) }
+            items.clear()
+            return listOf(MonkeySwap(trueMonkey, trueList), MonkeySwap(falseMonkey, falseList))
+        }
+
+        fun catchItems(newItems: List<BigInteger>) {
+            items += newItems
+        }
+
+    }
+
+    private fun List<String>.toMonkey(): Monkey {
+        val items = valueForTerm("Starting").split(", ").map { it.toBigInteger() }.toMutableList()
+        val test: Long = valueForTerm("Test").split(" by ").last().toLong()
+        val trueMonkey: Int = valueForTerm(" true").split(" monkey ").last().toInt()
+        val falseMonkey: Int = valueForTerm(" false").split(" monkey ").last().toInt()
+        val operationString: String = valueForTerm(" Operation").split("new = ").last()
+        return Monkey(
+            items = items,
+            operation = operationString.toOperation(),
+            test = { it.mod(test.toBigInteger()) == BigInteger.ZERO },
+            trueMonkey = trueMonkey,
+            falseMonkey = falseMonkey,
+            worryDrop = { it / BigInteger.valueOf(3) }
+        )
+    }
+
+    private fun List<String>.valueForTerm(term: String) = this
+        .first {
+            it.contains(term)
+        }
+        .split(": ").last()
+
+    private fun String.toOperation(): (BigInteger) -> BigInteger {
+        val term2String = this.split("old ").last().substring(2)
+        val term2:BigInteger = if (term2String == "old") BigInteger.ZERO else term2String.toBigInteger()
+        return when {
+            (this == "old * old") -> { x: BigInteger -> x * x }
+            (this.startsWith("old + ")) -> { x: BigInteger -> x + term2 }
+            (this.startsWith("old * ")) -> { x: BigInteger -> x * term2 }
+            else -> {
+                error("Oops!")
+            }
+        }
+
+    }
+
+    private fun extractMonkeys(input: String, worryDivisor: Int = 3): List<Monkey> =
+        input.split("\n\n").map { it.lines() }.map { it.toMonkey() }
+
+    override fun part1(input: String): BigInteger {
+        val monkeys: List<Monkey> = extractMonkeys(input)
+        // monkeys.inspect()
+        println("====== And Go!")
+        repeat(20) { round ->
+            //   println("Round $round")
+            monkeyRound(monkeys, true)
+            //   monkeys.inspect()
+        }
+        val topMonkeys = monkeys.map { it.inspectionCount }.sorted().takeLast(2)
+        return topMonkeys.first().toBigInteger() * topMonkeys.last().toBigInteger()
+    }
+
+    private fun monkeyRound(monkeys: List<Monkey>, doWorryDrop: Boolean) {
+        monkeys.forEachIndexed() { index, monkey ->
+            val swaps = monkey.round(doWorryDrop)
+//            println("----Before Throw----")
+//            monkey.inspect(index)
+//            swaps.inspect(index)
+            swaps.forEach { swap ->
+                monkeys[swap.first].catchItems(swap.second)
+            }
+
+        }
+    }
+
+    override fun part2(input: String): BigInteger {
+        val roundCheck = listOf(0, 1, 3, 20, 700,999,1999, 2999)// 19, 20, 21, 1999, 2999, 3999)
+        val monkeys: List<Monkey> = extractMonkeys(input, 1)
+        monkeys.inspect()
+        repeat(100) { round ->
+           // println("==============Round $round")
+            monkeyRound(monkeys, false)
+            //   monkeys.inspect()
+            if (round in roundCheck) {
+                println("== After Round $round ==")
+                monkeys.inspectCount()
+                monkeys.inspect()
+            }
+        }
+        println("***** Final result *****")
+        monkeys.inspectCount()
+        val topMonkeys = monkeys.map { it.inspectionCount }.sorted().takeLast(2)
+        return topMonkeys.first().toBigInteger() * topMonkeys.last().toBigInteger()
+    }
+
+    //================ inspect methods
+
+
+    private fun List<Monkey>.inspectCount() = this.forEachIndexed { index, monkey ->
+//        print("Monkey $index: true=${monkey.trueMonkey} false=${monkey.falseMonkey} ")
+        println("Monkey $index inspected items ${monkey.inspectionCount} times.")
+    }
+
+    private fun List<MonkeySwap>.inspect(index: Int) {
+        println("From $index True: throws to ${this[0].first}  items: ${this[0].second}")
+        println("From $index False: throws to ${this[1].first}  items: ${this[1].second}")
+    }
+
+    private fun List<Monkey>.inspect() = this.forEachIndexed { index, monkey ->
+//        print("Monkey $index: true=${monkey.trueMonkey} false=${monkey.falseMonkey} ")
+        monkey.inspect(index)
+    }
+
+    private  fun Monkey.inspect(index:Int) {
+        println("Monkey $index: inspected ${this.inspectionCount} ${this.items} ")
+    }
+}
+
---
+++ src/main/kotlin/days/MainStringBigInt.kt
@@ -0,0 +1,36 @@
+package days
+import readInputString
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val today = Day11
+    println("***** Day${today.number} *****")
+    val testInput = readInputString(today.number,"Day_test")
+    var part1Test:BigInteger = BigInteger.ZERO
+    val part1TestMillis = measureTimeMillis {
+        part1Test = today.part1(testInput)
+    }
+    check(part1Test == today.expectedPart1Test)
+    println("✅ one with ${today.expectedPart1Test} ✅ in $part1TestMillis ms")
+    var part2Test =  BigInteger.ZERO
+    val part2TestMillis = measureTimeMillis {
+        part2Test = today.part2(testInput)
+    }
+    check(part2Test == today.expectedPart2Test)
+    println("✅ two with ${today.expectedPart2Test} ✅ in $part2TestMillis ms")
+    println("*****************")
+
+    val input = readInputString(today.number, "Day")
+    var part1 = BigInteger.ZERO
+    val part1Millis = measureTimeMillis {
+        part1 = today.part1(input)
+    }
+    var part2 = BigInteger.ZERO
+    val part2Millis = measureTimeMillis {
+        part2 = today.part2(input)
+    }
+    println("🌟 one : $part1 in $part1Millis ms")
+    println("🌟 two : $part2 in $part2Millis ms")
+    println("*****************")
+}
