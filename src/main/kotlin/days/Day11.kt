21c6c21d16293c6eb3f99c5ec393ee60d4fcee5d
maia grotepass <maiatoday@gmail.com>
2022-12-11 13:31:02 +0200

    add the magic clamps but only 2/3 work

--- src/main/kotlin/days/Day11.kt
+++ src/main/kotlin/days/Day11.kt
@@ -11,16 +11,22 @@ object Day11 : Day<BigInteger, String> {
 
     data class Monkey(
         val items: MutableList<BigInteger>,
-        val operation: (BigInteger) -> (BigInteger),
+        val operation: (BigInteger, BigInteger) -> (BigInteger),
         val test: (BigInteger) -> (Boolean),
+        val testValue: BigInteger,
         val trueMonkey: Int,
         val falseMonkey: Int,
         val worryDrop: (BigInteger) -> (BigInteger) = { it / BigInteger.valueOf(3) },
         var inspectionCount: Long = 0
     ) {
-        fun round(doWorryDrop: Boolean = false): List<MonkeySwap> {
+        fun round(
+            doWorryDrop: Boolean = false,
+            magicClampNumber: BigInteger = BigInteger.ONE
+        ): List<MonkeySwap> {
 //            val itemValues = items.map { operation(it) }.map { if (doWorryDrop) worryDrop(it) else it }
-            val itemValues = if (doWorryDrop) items.map { operation(it) }.map { worryDrop(it)} else items.map { operation(it) }
+            val itemValues =
+                if (doWorryDrop) items.map { operation(it, magicClampNumber) }
+                    .map { worryDrop(it) } else items.map { operation(it, magicClampNumber) }
             inspectionCount += itemValues.size
             val trueList = itemValues.filter { test(it) }
             val falseList = itemValues.filter { !test(it) }
@@ -44,6 +50,7 @@ object Day11 : Day<BigInteger, String> {
             items = items,
             operation = operationString.toOperation(),
             test = { it.mod(test.toBigInteger()) == BigInteger.ZERO },
+            testValue = test.toBigInteger(),
             trueMonkey = trueMonkey,
             falseMonkey = falseMonkey,
             worryDrop = { it / BigInteger.valueOf(3) }
@@ -56,39 +63,41 @@ object Day11 : Day<BigInteger, String> {
         }
         .split(": ").last()
 
-    private fun String.toOperation(): (BigInteger) -> BigInteger {
+    private fun String.toOperation(): (BigInteger, BigInteger) -> BigInteger {
         val term2String = this.split("old ").last().substring(2)
-        val term2:BigInteger = if (term2String == "old") BigInteger.ZERO else term2String.toBigInteger()
+        val term2: BigInteger = if (term2String == "old") BigInteger.ZERO else term2String.toBigInteger()
         return when {
-            (this == "old * old") -> { x: BigInteger -> x * x }
-            (this.startsWith("old + ")) -> { x: BigInteger -> x + term2 }
-            (this.startsWith("old * ")) -> { x: BigInteger -> x * term2 }
+            (this == "old * old") -> { x: BigInteger, clamp: BigInteger -> if (x.divBy(clamp)) x else (x * x) }
+            (this.startsWith("old + ")) -> { x: BigInteger, _: BigInteger -> x + term2 }
+            (this.startsWith("old * ")) -> { x: BigInteger, _: BigInteger -> if (x.divBy(term2)) x else x * term2 }
             else -> {
                 error("Oops!")
             }
         }
-
     }
+
+    fun BigInteger.divBy(i: BigInteger): Boolean = this.mod(i) == BigInteger.ZERO
 
     private fun extractMonkeys(input: String, worryDivisor: Int = 3): List<Monkey> =
         input.split("\n\n").map { it.lines() }.map { it.toMonkey() }
 
     override fun part1(input: String): BigInteger {
         val monkeys: List<Monkey> = extractMonkeys(input)
+        val magicClampNumber = monkeys.map { it.testValue }.reduce { acc, i -> i * acc }
         // monkeys.inspect()
         println("====== And Go!")
         repeat(20) { round ->
             //   println("Round $round")
-            monkeyRound(monkeys, true)
+            monkeyRound(monkeys, true, magicClampNumber)
             //   monkeys.inspect()
         }
         val topMonkeys = monkeys.map { it.inspectionCount }.sorted().takeLast(2)
         return topMonkeys.first().toBigInteger() * topMonkeys.last().toBigInteger()
     }
 
-    private fun monkeyRound(monkeys: List<Monkey>, doWorryDrop: Boolean) {
+    private fun monkeyRound(monkeys: List<Monkey>, doWorryDrop: Boolean, magicClampNumber: BigInteger) {
         monkeys.forEachIndexed() { index, monkey ->
-            val swaps = monkey.round(doWorryDrop)
+            val swaps = monkey.round(doWorryDrop, magicClampNumber)
 //            println("----Before Throw----")
 //            monkey.inspect(index)
 //            swaps.inspect(index)
@@ -100,17 +109,18 @@ object Day11 : Day<BigInteger, String> {
     }
 
     override fun part2(input: String): BigInteger {
-        val roundCheck = listOf(0, 1, 3, 20, 700,999,1999, 2999)// 19, 20, 21, 1999, 2999, 3999)
+        val roundCheck = listOf(0, 1, 3, 4, 5, 6, 20, 700, 999, 1999, 2999)// 19, 20, 21, 1999, 2999, 3999)
         val monkeys: List<Monkey> = extractMonkeys(input, 1)
+        val magicClampNumber = monkeys.map { it.testValue }.reduce { acc, i -> i * acc }
         monkeys.inspect()
-        repeat(100) { round ->
-           // println("==============Round $round")
-            monkeyRound(monkeys, false)
-            //   monkeys.inspect()
+        repeat(2000) { round ->
+            // println("==============Round $round")
+            monkeyRound(monkeys, false, magicClampNumber)
+           // monkeys.inspect()
             if (round in roundCheck) {
                 println("== After Round $round ==")
-                monkeys.inspectCount()
-                monkeys.inspect()
+              //  monkeys.inspectCount()
+              //  monkeys.inspect()
             }
         }
         println("***** Final result *****")
@@ -137,7 +147,7 @@ object Day11 : Day<BigInteger, String> {
         monkey.inspect(index)
     }
 
-    private  fun Monkey.inspect(index:Int) {
+    private fun Monkey.inspect(index: Int) {
         println("Monkey $index: inspected ${this.inspectionCount} ${this.items} ")
     }
 }
