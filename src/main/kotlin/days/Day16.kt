498a25e4d450e9fc2dbf78cafe93a95a3f80da57
maia grotepass <maiatoday@gmail.com>
2022-12-16 23:18:41 +0200

    working dijkstra  but no more than that

--- src/main/kotlin/days/Day16.kt
+++ src/main/kotlin/days/Day16.kt
@@ -9,11 +9,50 @@ object Day16 : Day<Day16ReturnType, Day16InputType> {
     override val number: Int = 16
     override val expectedPart1Test: Day16ReturnType = 1651
     override val expectedPart2Test: Day16ReturnType = -1
+    override var useTestData = true
 
     override fun part1(input: Day16InputType): Day16ReturnType {
         val tunnelMap = input.toTunnelMap()
+        val valves = tunnelMap.keys.map { it.first }.toSet()
+        val valveFlows = tunnelMap.map { it.key.first to it.value }.toMap()
+        val shortestPaths = findShortestPaths(valves, tunnelMap.keys)
+        shortestPaths.forEach {
+            println("Reach ${it.key} in ${it.value.distance} steps prev valve ${it.value.previousValve}")
+        }
         return expectedPart1Test
     }
+
+    private fun findShortestPaths(
+        valves: Set<String>,
+        tunnelMap: Set<Tunnel>
+    ): MutableMap<String, TableEntry> {
+        val valvesToVisit = mutableSetOf<String>().also { it.addAll(valves) }
+        val visited = mutableListOf<String>()
+
+        val table = mutableMapOf<String, TableEntry>()
+        valves.forEach {
+            table[it] = TableEntry(Int.MAX_VALUE, "")
+        }
+        table[valves.first()] = TableEntry(0, "")
+
+        while (valvesToVisit.isNotEmpty()) {
+            val currentValve = table.filter { it.key in valvesToVisit }.minBy { it.value.distance }.key
+            valvesToVisit.remove(currentValve)
+            val distance = table[currentValve]?.distance ?: Int.MAX_VALUE
+            tunnelMap.filter { it.first == currentValve }.forEach {
+                // all edges are weighted 1  for now
+                val newDistance: Int = distance + 1
+                val tableDistance: Int = table[it.second]?.distance ?: Int.MAX_VALUE
+                if (tableDistance > newDistance) {
+                    table[it.second] = TableEntry(newDistance, currentValve)
+                }
+            }
+            visited.add(currentValve)
+        }
+        return table
+    }
+
+    data class TableEntry(val distance: Int, val previousValve: String)
 
     override fun part2(input: Day16InputType): Day16ReturnType {
         return expectedPart2Test
@@ -30,13 +69,7 @@ object Day16 : Day<Day16ReturnType, Day16InputType> {
             .matchEntire(this)
             ?.destructured
             ?: throw IllegalArgumentException("Bad data $this")
-        val toList = to.split(", ")
-        val tunnelList = buildList {
-            for (t in toList) {
-                this.add(from to t to rate.toInt())
-            }
-        }
-        return tunnelList
+        return to.split(", ").map { toTunnel -> from to toTunnel to rate.toInt() }
     }
 
     private fun List<String>.toTunnelMap(): Map<Tunnel, Int> =
--- src/test/kotlin/days/DayTest.kt
+++ src/test/kotlin/days/DayTest.kt
@@ -1,6 +1,6 @@
 package days
 
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert
 import org.junit.Test
 import readInput
@@ -8,14 +8,40 @@ import readInput
 class DayTest {
     private val day = Day16
     private val testInput = readInput(day.number,"Day_test")
-//    private val testInput = readInput(day.number,"Day")
+    private val puzzleInput = readInput(day.number,"Day")
     @Test
     fun part1() {
-        MatcherAssert.assertThat(day.part1(testInput), CoreMatchers.`is`(day.expectedPart1Test))
+        day.useTestData = true
+        MatcherAssert.assertThat(day.part1(testInput), `is`(day.expectedPart1Test))
     }
 
     @Test
     fun part2() {
-        MatcherAssert.assertThat(day.part2(testInput), CoreMatchers.`is`(day.expectedPart2Test))
+        day.useTestData = true
+        MatcherAssert.assertThat(day.part2(testInput), `is`(day.expectedPart2Test))
+    }
+
+    @Test
+    fun solve1() {
+        day.useTestData = false
+        val solution1 = day.part1(puzzleInput)
+        println("Day${day.number} Part 1 ⭐️")
+//        val wrongValue = 0
+//        MatcherAssert.assertThat(day.part2(testInput), not(wrongValue))
+//        MatcherAssert.assertThat(day.part2(testInput), `is`(solution1 < wrongValue))
+//        MatcherAssert.assertThat(day.part2(testInput), `is`(solution1 > wrongValue))
+        println(solution1)
+    }
+
+    @Test
+    fun solve2() {
+        day.useTestData = false
+        val solution2 = day.part2(puzzleInput)
+        println("Day${day.number} Part 2 ⭐️⭐️")
+//        val wrongValue = 0
+//        MatcherAssert.assertThat(day.part2(testInput), not(wrongValue))
+//        MatcherAssert.assertThat(day.part2(testInput), `is`(solution2 < wrongValue))
+//        MatcherAssert.assertThat(day.part2(testInput), `is`(solution2 > wrongValue))
+        println(solution2)
     }
 }
