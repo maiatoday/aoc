565c869c6b2dc5d8bc7e51ee4db577502afee610
maia grotepass <maiatoday@gmail.com>
2022-12-18 21:12:14 +0200

    empty  day 18

---
+++ src/main/kotlin/days/Day18.kt
@@ -0,0 +1,19 @@
+package days
+
+typealias Day18ReturnType = Int
+typealias Day18InputType = List<String>
+
+object Day18 : Day<Day18ReturnType, Day18InputType> {
+    override val number: Int = 18
+    override val expectedPart1Test: Day18ReturnType = -1
+    override val expectedPart2Test: Day18ReturnType = -1
+    override var useTestData = true
+
+    override fun part1(input: Day18InputType): Day18ReturnType {
+        return expectedPart1Test
+    }
+
+    override fun part2(input: Day18InputType): Day18ReturnType {
+        return expectedPart2Test
+    }
+}
