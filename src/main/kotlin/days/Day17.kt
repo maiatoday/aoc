4de84d50b1a3ca511b0d57f9de9a29c627b6f1d2
maia grotepass <maiatoday@gmail.com>
2022-12-20 20:49:45 +0200

    Day  17 part 1 at last 🟥🟥🟥🟥
    some cleanup

--- src/main/kotlin/days/Day17.kt
+++ src/main/kotlin/days/Day17.kt
@@ -9,11 +9,11 @@ typealias Day17InputType = String
 object Day17 : Day<Day17ReturnType, Day17InputType> {
     override val number: Int = 17
     override val expectedPart1Test: Day17ReturnType = 3068
-    override val expectedPart2Test: Day17ReturnType = -1
+    override val expectedPart2Test: Day17ReturnType = -1 // 1514285714288
     override var useTestData = true
 
     const val CAVE_WIDTH = 7
-    val debug = false
+    const val debug = false
 
     override fun part1(input: Day17InputType): Day17ReturnType {
         val jets = input.map { c ->
@@ -29,7 +29,6 @@ object Day17 : Day<Day17ReturnType, Day17InputType> {
         var jetIndex = 0
         var fillCount = 0
         repeat(2022) {
-            print("turn ***** $turn")
             val height = cave.highestRock()
             val rock = when (turn % 5) {
                 0 -> Flat(height)
@@ -39,27 +38,23 @@ object Day17 : Day<Day17ReturnType, Day17InputType> {
                 4 -> Block(height)
                 else -> error("Oops missing block type")
             }
-            println(" Starting rock ~~~~~${rock.name} ${rock.position}")
 
             while (rock.canMove) {
                 val jet = jets[jetIndex]
-                val jetstring = if (jet > 0) ">" else "<"
-                if (debug) println("~~~~~~~$jet  $jetstring")
+                val jetDirection = if (jet > 0) ">" else "<"
+                if (debug) println("~~~~~~~$jet  $jetDirection")
                 rock.jet(jet, cave)
                 jetIndex = (jetIndex + 1) % jets.size
                 rock.down(cave)
                 if (debug) println("down ~~~~~$rock")
             }
-            fillCount += rock.fill
-            val actualFill = cave.sumOf {
-                it.count { c -> c == '#' }
-            }
-            println("Fill count actual $actualFill expected $fillCount")
+//            fillCount += rock.fill
+//            val actualFill = cave.sumOf {
+//                it.count { c -> c == '#' }
+//            }
             turn++
-            // cave.debug()
         }
 
-        //   cave.debug2()
         return cave.highestRock()
     }
 
@@ -69,32 +64,12 @@ object Day17 : Day<Day17ReturnType, Day17InputType> {
 
     //----------------------------------------------------------
 
-    private fun List<String>.debug2() {
-        val highest = this.highestRock()
-        println("========== highest rock = $highest ======")
-        for (y in highest - 1 downTo 500) {
-            print("|")
-            print(this[y])
-            println("|")
-        }
-    }
-
-    private fun List<String>.debug() {
-        val highest = this.highestRock()
-        println("========== highest rock = $highest ======")
-        for (y in highest - 1 downTo 0) {
-            print("|")
-            print(this[y])
-            println("|")
-        }
-    }
-
     private fun List<String>.highestRock() = this.size
 
-    sealed class Shape(height: Int, val name: String, private val shape: List<String>, val fill: Int) {
+    sealed class Shape(height: Int, private val name: String, private val shape: List<String>, val fill: Int) {
         private val start = Point(2, 3 + height)
         var canMove: Boolean = true
-        var position: Point = start
+        private var position: Point = start
         private val width = shape[0].length
         private val height = shape.size
 
@@ -112,41 +87,31 @@ object Day17 : Day<Day17ReturnType, Day17InputType> {
             if (y >= cave.size) return false // still falling through space
 
             //  compare whole block because of +
-            val shapeBlocks = shape.map { if (direction == 1) it[width - 1] else it[0] }
-            val rowBlocks =
-                (y..y + height).mapNotNull {
-                    if (it < cave.size) {
-                        if (direction == 1) {
-                            cave[it].substring(x + width - 1, x + width - 1 + 1).first()
-                        } else {
-                            cave[it].substring(x, x + 1).first()
-                        }
-                    } else null
-                }
-            val combo = shapeBlocks.zip(rowBlocks)
+            val caveXRange = (x until x + width)
+            val caveYRange = y..min(y + height - 1, cave.size - 1)
+            val shapeBlocks = shape.flatMap { it.toList() }
+            val caveRanges = getCaveRanges(caveYRange, caveXRange)
+            val caveBlocks = caveRanges.map { cave[it.first][it.second] }
+            val combo = shapeBlocks.zip(caveBlocks)
             return combo.any { it.first == '#' && it.second == '#' }
         }
 
         fun down(cave: MutableList<String>) {
             val newY = position.y - 1
             val canGoDown = if (newY >= cave.size) {
+                // falling through space
                 true
             } else if (newY < 0) {
+                // hit the floor
                 false
-            } else if (this is Plus && newY < cave.size - 1) {
-                // have to check the whole of the block for all the blocks not just only plus
-                val secondRow = shape[1]
-                val rowBlocks = cave[newY + 1].substring(position.x, position.x + width)
-                val combo = secondRow.zip(rowBlocks)
-                combo.none { it.first == '#' && it.second == '#' }
-            } else if (newY <= cave.size - 1) {
-                val shapeBlocks = shape.first()
-                val rowBlocks = cave[newY].substring(position.x, position.x + width)
-                val combo = shapeBlocks.zip(rowBlocks)
-                /// the borkiness is here because of the godforsaken plus TODO check the  second line
-                combo.none { it.first == '#' && it.second == '#' }
             } else {
-                true
+                val shapeBlocks = shape.flatMap { it.toList() }
+                val caveXRange = position.x until position.x + width
+                val caveYRange = newY..min(newY + height - 1, cave.size - 1)
+                val caveRanges = getCaveRanges(caveYRange, caveXRange)
+                val caveBlocks = caveRanges.map { cave[it.first][it.second] }
+                val combo = shapeBlocks.zip(caveBlocks)
+                combo.none { it.first == '#' && it.second == '#' }
             }
 
             if (canGoDown) {
@@ -174,9 +139,9 @@ object Day17 : Day<Day17ReturnType, Day17InputType> {
         private fun replaceLine(l: String, s: String): String =
             l.zip(s).map {
                 when {
-                    (it.first == '_' && it.second == '_') -> '_'
-                    (it.first == '_' && it.second == '#') -> '#'
-                    (it.first == '#' && it.second == '_') -> '#'
+                    (it.first == '~' && it.second == '~') -> '~'
+                    (it.first == '~' && it.second == '#') -> '#'
+                    (it.first == '#' && it.second == '~') -> '#'
                     else -> error("bad replace undetected collision")
                 }
             }.joinToString("")
@@ -185,7 +150,7 @@ object Day17 : Day<Day17ReturnType, Day17InputType> {
             l.substring(0, x) + s + l.substring(x + s.length)
 
         private fun buildLine(s: String, x: Int): String =
-            concatLine("_".repeat(CAVE_WIDTH), s, x)
+            concatLine("~".repeat(CAVE_WIDTH), s, x)
 
 
         override fun toString(): String =
@@ -193,14 +158,31 @@ object Day17 : Day<Day17ReturnType, Day17InputType> {
 
     }
 
+    private fun getCaveRanges(
+        caveYRange: IntRange,
+        caveXRange: IntRange
+    ) = caveYRange.flatMap { i -> caveXRange.map { j -> i to j } }
+
     class Flat(height: Int) : Shape(height, "Flat", listOf("####"), 4)
 
-    class Plus(height: Int) : Shape(height, "Plus", listOf("_#_", "###", "_#_"), 5)
+    class Plus(height: Int) : Shape(height, "Plus", listOf("~#~", "###", "~#~"), 5)
 
-    class Ell(height: Int) : Shape(height, "Ell", listOf("###", "__#", "__#"), 5)
+    class Ell(height: Int) : Shape(height, "Ell", listOf("###", "~~#", "~~#"), 5)
 
     class Line(height: Int) : Shape(height, "Line", listOf("#", "#", "#", "#"), 4)
 
     class Block(height: Int) : Shape(height, "Block", listOf("##", "##"), 4)
+
+    private fun List<String>.debug() {
+        if (debug) {
+            val highest = this.highestRock()
+            println("========== highest rock = $highest ======")
+            for (y in highest - 1 downTo 0) {
+                print("|")
+                print(this[y])
+                println("|")
+            }
+        }
+    }
 
 }
