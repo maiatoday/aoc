4188475a19e80d1d6787922ca51a63415ccfeb91
maia grotepass <maiatoday@gmail.com>
2022-12-30 08:47:31 +0200

    lDay  16: look  at not creating state allocations

--- src/main/kotlin/days/Day16.kt
+++ src/main/kotlin/days/Day16.kt
@@ -10,7 +10,7 @@ object Day16 : Day<Day16ReturnType, Day16InputType> {
     override val expectedPart1Test: Day16ReturnType = 1651
     override val expectedPart2Test: Day16ReturnType = 1707
     override var useTestData = true
-    override val debug = false
+    override val debug = true
 
     override fun part1(input: Day16InputType): Day16ReturnType {
         val tunnelMap = input.toTunnelMap()
@@ -24,10 +24,31 @@ object Day16 : Day<Day16ReturnType, Day16InputType> {
     }
 
     //--------------------------------------------------
+
+    data class OneState(
+        var valve: String,
+        var time: Int,
+        var openedValves: List<String>,
+        var elephantHelp: Boolean) {
+
+//        var valve: String =""
+//        var time: Int =0
+//        var openedValves: List<String>  = emptyList()
+//        var elephantHelp: Boolean = false
+
+        fun set(v: String, t: Int, ov: List<String>, e: Boolean) {
+            valve = v
+            time = t
+            openedValves = ov
+            elephantHelp = e
+        }
+    }
+    val oneState = OneState("", 0, emptyList(), false)
+
     data class State(val valve: String, val time: Int, val openedValves: List<String>, val elephantHelp: Boolean)
 
     private val memoizedStates =
-        HashMap<Int, Int>(87000000) // super rough ballpark of capacity to  stop hashmap growing all the time
+        HashMap<State, Int>(87000000) // super rough ballpark of capacity to  stop hashmap growing all the time
 
     private fun maxFlow(
         currentValve: String,
@@ -38,7 +59,14 @@ object Day16 : Day<Day16ReturnType, Day16InputType> {
     ): Int {
         // counting down from max time to 0, i.e. time left
         if (time == 0) return if (elephantHelp) maxFlow("AA", 26, openedValves, false, tunnels) else 0
+        oneState.set(currentValve, time, openedValves, elephantHelp)
         val state = State(currentValve, time, openedValves, elephantHelp)
+        log {
+//            oneState.set(currentValve, time, openedValves, elephantHelp)
+//            check(oneState.hashCode() == state.hashCode())
+//            println("oneState ${oneState.hashCode()}")
+//            println("state ${state.hashCode()}")
+        }
         var answer = 0
         if (state in memoizedStates) {
             answer = memoizedStates[state] ?: error("oops I thought I remembered something $state")
