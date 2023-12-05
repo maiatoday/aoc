7562f5c60eadb5330850e6180eeb5980ecc88663
maiatoday <maiatoday@gmail.com>
2023-12-05 13:01:08 +0200

    Day 05: 🌱 part 1

---
+++ src/MainString.kt
@@ -0,0 +1,37 @@
+package days
+
+import util.readInputString
+import kotlin.system.measureTimeMillis
+
+typealias MainStringReturnType = Long
+fun main() {
+    val today = Day05
+    println("***** Day${today.number} *****")
+    val testInput = readInputString(today.number,"Day_test")
+    var part1Test:MainStringReturnType
+    val part1TestMillis = measureTimeMillis {
+        part1Test = today.part1(testInput)
+    }
+    check(part1Test == today.expectedPart1Test)
+    println("✅ one with ${today.expectedPart1Test} ✅ in $part1TestMillis ms")
+    var part2Test:MainStringReturnType
+    val part2TestMillis = measureTimeMillis {
+        part2Test = today.part2(testInput)
+    }
+    check(part2Test == today.expectedPart2Test)
+    println("✅ two with ${today.expectedPart2Test} ✅ in $part2TestMillis ms")
+    println("*****************")
+
+    val input = readInputString(today.number, "Day")
+    var part1:MainStringReturnType
+    val part1Millis = measureTimeMillis {
+        part1 = today.part1(input)
+    }
+    var part2:MainStringReturnType
+    val part2Millis = measureTimeMillis {
+        part2 = today.part2(input)
+    }
+    println("🌟 one : $part1 in $part1Millis ms")
+    println("🌟 two : $part2 in $part2Millis ms")
+    println("*****************")
+}
--- src/days/Day05.kt
+++ src/days/Day05.kt
@@ -1,54 +1,55 @@
 package days
 
 
-object Day05 : Day<Long, List<String>> {
+object Day05 : Day<Long, String> {
     override val number: Int = 5
-    override val expectedPart1Test: Long = -1L
+    override val expectedPart1Test: Long = 35L
     override val expectedPart2Test: Long = -1L
     override var useTestData = true
     override val debug = false
 
-    override fun part1(input: List<String>): Long {
-        val (seeds, category) = readAlamanac(input)
-        return seeds.minOf { seed ->
-            category.fold(seed) { l, t -> t.convert(l) }
+    override fun part1(input: String): Long {
+        val (seeds, categories) = readAlmanac(input)
+        val positions = seeds.map { seed ->
+            categories.fold(seed) { l, c -> c.convert(l) }
         }
+        println(positions)
+        return positions.min()
     }
 
-    override fun part2(input: List<String>): Long {
+    override fun part2(input: String): Long {
         return -1L
     }
 
-    private fun readAlamanac(input: List<String>): Pair<List<Long>, List<Category>> {
-        val seeds = input[0].readLongs()
-//        val tables = buildList<Table> {
-//            input.drop(2)
-//                .map            
-//        }
-//        input.drop(2).
-        return seeds to emptyList()
+    private fun readAlmanac(input: String): Pair<List<Long>, List<Category>> {
+        val inputLines = input.split("\n\n").map { it.lines() }
+        val seeds = inputLines[0][0].readLongs()
+        val categories = inputLines.drop(1).map {
+            Category(it)
+        }
+        return seeds to categories
     }
 
-    data class Category(val tables: List<Table>) {
+    data class Category(val name: String, val tables: List<Table>) {
         fun convert(source: Long): Long {
             val match = tables.filter { source in it.sourceRange }
             return if (match.size == 1) {
                 val (sourceRange, destinationOffset) = match.first()
-                destinationOffset + sourceRange.last - source
+                destinationOffset + source - sourceRange.first
             } else {
                 source
             }
         }
     }
 
-    fun Category(input: List<String>): Category =
-            Category(input.map { Table(it) })
+    private fun Category(input: List<String>): Category =
+            Category(input[0], input.drop(1).filter { it.isNotEmpty() }.map { Table(it) })
 
     data class Table(val sourceRange: LongRange, val destinationOffset: Long)
 
-    fun Table(input: String): Table {
+    private fun Table(input: String): Table {
         val (destinationOffset, sourceStart, sourceLength) = input.readLongs()
-        return Table(sourceStart..sourceStart + sourceLength, destinationOffset)
+        return Table(sourceStart..<sourceStart + sourceLength, destinationOffset)
     }
 
     private fun String.readLongs() = Regex("""\d+""").findAll(this)
