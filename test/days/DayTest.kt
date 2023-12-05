52d82d39e9ea4ec6e5b3633f1070fb6d360bb19e
maiatoday <maiatoday@gmail.com>
2023-12-05 13:00:37 +0200

    Test fixup

--- test/days/DayTest.kt
+++ test/days/DayTest.kt
@@ -1,29 +1,28 @@
 package days
 
 import util.readInput
+import util.readInputString
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class DayTest {
-    private val dayNumber = 99
-    private val day = Day99
-    private val dayTestAnswer1 = -1L
-    private val dayTestAnswer2 = -1L
-    private val testInput = readInput(dayNumber, "Day_test")
-    
+    private val dayNumber = 5
+    private val day = Day05
+//    private val testInput = readInput(dayNumber, "Day_test")
+
     //    private val testInput = readInput(day.number,"Day")
     //    private val puzzleInput = readInput(day.number, "Day")
 
-    //    private val testInputString = readInputString(day.number,"Day_test")
+    private val testInputString = readInputString(day.number, "Day_test")
     //    private val puzzleInputString = readInputString(day.number,"Day")
 
     @Test
     fun part1() {
-        assertEquals(day.expectedPart1Test, day.part1(testInput))
+        assertEquals(day.expectedPart1Test, day.part1(testInputString))
     }
 
     @Test
     fun part2() {
-        assertEquals(day.expectedPart2Test, day.part2(testInput))
+        assertEquals(day.expectedPart2Test, day.part2(testInputString))
     }
 }
