d1fae8186058fc8aa99d392ec76fa48c3e076bd4
Maia Grotepass <maiatoday@gmail.com>
2023-12-11 09:03:09 +0200

    Day 11: ✨✨

--- src/days/Day11.kt
+++ src/days/Day11.kt
@@ -5,55 +5,44 @@ import kotlin.math.abs
 object Day11 : Day<Long, List<String>> {
     override val number: Int = 11
     override val expectedPart1Test: Long = 374L
-    override val expectedPart2Test: Long = -1L
+    override val expectedPart2Test: Long = 82000210L
     override var useTestData = true
     override val debug = false
 
     override fun part1(input: List<String>): Long {
-        val expandedSpace = expand(input)
-        val galaxies = findGalaxies(expandedSpace)
-        val doublePaths = galaxies.flatMap { g -> (galaxies - g).map { g.shortestPath(it) } }.sum().toLong()
+        val galaxies = findGalaxies(input, 2)
+        val doublePaths = galaxies.flatMap { g -> (galaxies - g).map { g.shortestPath(it) } }.sum()
         return doublePaths / 2 // 1->2 is the same as 2->1
     }
 
-    private fun expand(input: List<String>): List<String> = buildList {
-        val blankColumns = input[0].indices.filter { c -> input.all { r -> r[c] == '.' } }
-        val blankRow = ".".repeat(input[0].length + blankColumns.size)
-        input.map { row ->
-            if ("#" !in row) {
-                add(blankRow)
-                add(blankRow)
-            } else {
-                val expandedColumns = buildString(blankRow.length) {
-                    row.mapIndexed { c, space ->
-                        this@buildString.append(space)
-                        if (c in blankColumns) this@buildString.append(space)
-
-                    }
-                }
-                add(expandedColumns)
-            }
-        }
-    }
-
-    private fun findGalaxies(input: List<String>): List<Galaxy> = buildList {
+    private fun findGalaxies(input: List<String>, rate: Int): List<Galaxy> = buildList {
+        val blankColumns = input.findEmptyColumns()
+        val blankRows = input.findEmptyRows()
         var count = 1
+        var expandedRow = 0L
         input.mapIndexed { r, s ->
+            var expandedColumn = 0L
             s.mapIndexed { c, b ->
                 if (b == '#') {
-                    add(Galaxy(count, r, c))
+                    add(Galaxy(count, expandedRow, expandedColumn))
                     count++
                 }
+                if (c in blankColumns) expandedColumn += rate else expandedColumn++
             }
+            if (r in blankRows) expandedRow += rate else expandedRow++
         }
     }
 
+    private fun List<String>.findEmptyColumns() = this[0].indices.filter { c -> this.all { r -> r[c] == '.' } }
+    private fun List<String>.findEmptyRows() = this.mapIndexed { i, r -> (i to r) }.filter { "#" !in it.second }.map { it.first }
 
-    data class Galaxy(val id: Int, val row: Int, val column: Int) {
+    data class Galaxy(val id: Int, val row: Long, val column: Long) {
         fun shortestPath(other: Galaxy) = abs(row - other.row) + abs(column - other.column)
     }
 
     override fun part2(input: List<String>): Long {
-        return -1L
+        val galaxies = findGalaxies(input, 1000000)
+        val doublePaths = galaxies.flatMap { g -> (galaxies - g).map { g.shortestPath(it) } }.sum()
+        return doublePaths / 2 // 1->2 is the same as 2->1
     }
 }
