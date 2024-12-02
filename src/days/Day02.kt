532c67ad30f95ec139a8c9a27cfbdc1c0020577b
Maia Grotepass <maiatoday@gmail.com>
2024-12-02 08:08:59 +0200

    part 2 tests but fails data

--- src/days/Day02.kt
+++ src/days/Day02.kt
@@ -13,37 +13,57 @@ object Day02 : Day<Long, List<String>> {
     override fun part1(input: List<String>): Long {
         val reports = input.parse()
         val safe = reports.count {
-            it.isSafe()
+            it.isSafe().first
         }
         return safe.toLong()
     }
 
     override fun part2(input: List<String>): Long {
-        return -1L
+        val reports = input.parse()
+        val safe = reports.count {
+            it.isSafeWithDamp()
+        }
+        return safe.toLong()
     }
 
     fun part1Alt(input: List<String>): Long = -1L
     fun part2Alt(input: List<String>): Long = -1L
 
     private fun List<String>.parse() = this.map { s -> s.readInts() }
-    private fun List<Int>.isSafe(): Boolean {
+    private fun List<Int>.isSafeWithDamp(problemDampLevel: Int = 1): Boolean {
+        val (isSafe, errorIndex) = isSafe()
+        return if (!isSafe) {
+            val newList = toMutableList()
+            newList.removeAt(errorIndex)
+            newList.isSafe().first
+        } else {
+            true
+        }
+    }
+
+    private fun List<Int>.isSafe(): Pair<Boolean, Int> {
         var isSafe = true
         val increasing = (this[1] > this[0])
+        var errorIndex = size
         for (i in 1..<size) {
             if (increasing) {
                 if (this[i] <= this[i - 1]) {
                     isSafe = false
+                    errorIndex = i
                     break
                 }
             } else if (this[i] >= this[i - 1]) {
                 isSafe = false
+                errorIndex = i
                 break
             }
             if (abs(this[i] - this[i - 1]) !in 1..3) {
                 isSafe = false
+                errorIndex = i
                 break
             }
+
         }
-        return isSafe
+        return isSafe to errorIndex
     }
 }
