469dab819ecc0d5dee3a18d9a9948181d7fe0365
Maia Grotepass <maiatoday@gmail.com>
2024-12-02 20:36:12 +0200

    ☢️🔴🎚️brute force and cleanup

--- src/days/Day02.kt
+++ src/days/Day02.kt
@@ -2,6 +2,7 @@ package days
 
 import util.readInts
 import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 object Day02 : Day<Long, List<String>> {
     override val number: Int = 2
@@ -12,13 +13,14 @@ object Day02 : Day<Long, List<String>> {
 
     override fun part1(input: List<String>): Long =
         input.parse().count {
-            it.isSafe()
+            isSafe(it)
+            //isSafeFunc(it)
         }.toLong()
 
     override fun part2(input: List<String>): Long {
         val reports = input.parse()
         val safe = reports.count {
-            it.isSafeWithDampBrute()
+            isSafeWithDampBrute(it)
         }
         return safe.toLong()
     }
@@ -28,34 +30,38 @@ object Day02 : Day<Long, List<String>> {
 
     private fun List<String>.parse() = this.map { s -> s.readInts() }
 
-    private fun List<Int>.isSafeWithDampBrute(): Boolean {
-        val isSafe = isSafe()
-        val allSafeList = List(size) { i ->
-            val newList = this.toMutableList()
-            newList.removeAt(i)
-            newList.isSafe()
+    private fun isSafeWithDampBrute(levels: List<Int>): Boolean {
+        val isSafe = isSafe(levels)
+        val allSafeList = List(levels.size) { i ->
+            val newList = levels.toMutableList().apply { removeAt(i) }
+            isSafe(newList)
         } + isSafe
         return allSafeList.any { it }
     }
 
-    private fun List<Int>.isSafe(): Boolean {
-        var isSafe = true
-        val increasing = (this[1] > this[0])
-        for (i in 1..<size) {
+    private fun isSafe(levels: List<Int>): Boolean {
+        val increasing = (levels[1] > levels[0])
+        for (i in 1..<levels.size) {
             if (increasing) {
-                if (this[i] <= this[i - 1]) {
-                    isSafe = false
-                    break
+                if (levels[i] <= levels[i - 1]) {
+                    return false
                 }
-            } else if (this[i] >= this[i - 1]) {
-                isSafe = false
-                break
+            } else if (levels[i] >= levels[i - 1]) {
+                return false
             }
-            if (abs(this[i] - this[i - 1]) !in 1..3) {
-                isSafe = false
-                break
+            if ((levels[i] - levels[i - 1]).absoluteValue !in 1..3) {
+                return false
             }
         }
-        return isSafe
+        return true
     }
+
+    // a functional version that is possibly less readable
+    private fun isSafeFunc(levels: List<Int>): Boolean {
+        val increasing = (levels[1] > levels[0])
+        return levels.zipWithNext().map { (a, b) ->
+            (abs(b - a) in 1..3) && increasing == (b > a)
+        }.all { it }
+    }
+
 }
