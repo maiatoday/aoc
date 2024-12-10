9f65916b224dae5ac6af444c3b7415a06854527a
Maia Grotepass <maiatoday@gmail.com>
2024-12-10 18:10:04 +0200

    🗺️🔥🥾⭐️⭐️golfed it a bit

--- src/days/Day10.kt
+++ src/days/Day10.kt
@@ -20,28 +20,25 @@ object Day10 : Day<Long, List<String>> {
     fun findTrailScores(map: Map<Point, Int>, area: Area, part2: Boolean = false): Int =
         map.filter { it.value == START }.map { it.key }
             .map {
-                if (part2) it to search(it, START + 1, map, area)
-                else it to search(it, START + 1, map, area).distinct()
-            }.sumOf { it.second.size }
-
+                if (part2) search(it, START + 1, map, area)
+                else search(it, START + 1, map, area).distinct()
+            }.sumOf { it.size }
 
     fun search(p: Point, value: Int, map: Map<Point, Int>, area: Area): List<Point> =
         if (value <= END) {
             buildList {
-                val ppp =
+                addAll(
                     p.neighbours(maxX = area.xRange.last + 1, maxY = area.yRange.last + 1) //neighbours use < NOT <=
                         .filter { map[it] == value }
                         .flatMap { search(it, value + 1, map, area) }
-                addAll(ppp)
+                )
             }
         } else {
             if (map[p] == END) listOf(p) else emptyList()
         }
 
     fun parse(input: List<String>): Map<Point, Int> = buildMap {
-        for (y in input.indices) for (x in input[0].indices) {
-            if (input[y][x] == '.') put(Point(x, y), -1)
-            else put(Point(x, y), input[y][x].digitToInt())
-        }
+        for (y in input.indices) for (x in input[0].indices)
+            put(Point(x, y), input[y][x].digitToInt())
     }
 }
