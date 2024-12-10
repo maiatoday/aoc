0fd509ad97c4d155ed04062cd3ad7f4490615cc2
Maia Grotepass <maiatoday@gmail.com>
2024-12-10 17:52:12 +0200

    🗺️🔥🥾⭐️⭐️cleaned up

--- src/days/Day10.kt
+++ src/days/Day10.kt
@@ -2,7 +2,6 @@ package days
 
 import util.Area
 import util.Point
-import util.filterComments
 import util.neighbours
 
 object Day10 : Day<Long, List<String>> {
@@ -12,59 +11,37 @@ object Day10 : Day<Long, List<String>> {
     override var useTestData = true
     override val debug = false
 
+    override fun part1(input: List<String>): Long = findTrailScores(parse(input), Area(input)).toLong()
+
+    override fun part2(input: List<String>): Long = findTrailScores(parse(input), Area(input), true).toLong()
+
     const val START = 0
     const val END = 9
-    override fun part1(input: List<String>): Long {
-        val map = parse(input.filterComments())
-        val area = Area(input)
-        val scores = findTrailScores(map, area)
-        return scores.toLong()
-    }
+    fun findTrailScores(map: Map<Point, Int>, area: Area, part2: Boolean = false): Int =
+        map.filter { it.value == START }.map { it.key }
+            .map {
+                if (part2) it to search(it, START + 1, map, area)
+                else it to search(it, START + 1, map, area).distinct()
+            }.sumOf { it.second.size }
 
-    fun findTrailScores(map: Map<Point, Int>, area: Area, part2: Boolean = false): Int {
-        val possible = map.filter { it.value == START }.map { it.key }
-        if (part2) {
-            val answer = possible.map {
-                it to search(it, START + 1, map, area)
-            }
-            val zz = answer
-                .sumOf { it.second.size }
-            return zz
-        } else {
-            val answer = possible.map {
-                it to search(it, START + 1, map, area).distinct()
-            }
-            val zz = answer
-                .sumOf { it.second.size }
-            return zz
-        }
-    }
 
     fun search(p: Point, value: Int, map: Map<Point, Int>, area: Area): List<Point> =
         if (value <= END) {
             buildList {
-                val ppp = p.neighbours(maxX = area.xRange.last + 1, maxY = area.yRange.last + 1)
-                val nnn = ppp.filter { map[it] == value }
-                val zzz = nnn.flatMap { search(it, value + 1, map, area) }
-                addAll(zzz)
+                val ppp =
+                    p.neighbours(maxX = area.xRange.last + 1, maxY = area.yRange.last + 1) //neighbours use < NOT <=
+                        .filter { map[it] == value }
+                        .flatMap { search(it, value + 1, map, area) }
+                addAll(ppp)
             }
-            //  }
         } else {
             if (map[p] == END) listOf(p) else emptyList()
         }
-
 
     fun parse(input: List<String>): Map<Point, Int> = buildMap {
         for (y in input.indices) for (x in input[0].indices) {
             if (input[y][x] == '.') put(Point(x, y), -1)
             else put(Point(x, y), input[y][x].digitToInt())
         }
-    }
-
-    override fun part2(input: List<String>): Long {
-        val map = parse(input)
-        val area = Area(input)
-        val scores = findTrailScores(map, area, true)
-        return scores.toLong()
     }
 }
